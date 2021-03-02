8512609a89b11f49eb0b496b36c25b122f091b21
Muriithi_Kabogo <dmuriithi.k@gmail.com>
2021-03-02 20:27:23 +0300

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,39 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ char *argstostr(int ac, char **av)                                                                                                            
+{                                                                                                                                             
+char *concat;                                                                                                                               
+  int i1 = 0, i2 = 0, length1 = 0, length2 = 0;                                                                                               
+                                                                                                                                              
+  if (ac == 0 || av == NULL)                                                                                                                  
+    return (NULL);                                                                                                                            
+                                                                                                                                              
+  while (i1 < ac)                                                                                                                             
+    {                                                                                                                                         
+     length1 += strlen(av[i1]);                                                                                                              
+     i1++;                                                                                                                                    
+    }                                                                                                                                         
+  concat = malloc((sizeof(char) * length1) + 1);                                                                                              
+  if (concat == NULL)                                                                                                                         
+    return (NULL);  
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+ i1 = 0; 
+
+  while (i1 < ac)                                                                                                                             
+    {   
+
+    length2 = 0;
+
+      while (av[i1][length2])                                                                                                                 
+        {                                                                                                                                     
+      concat[i2] = av[i1][length2];                                                                                                           
+      length2++;                                                                                                                              
+      i2++;                                                                                                                                   
+        } 
+
+      concat[i2] = '\n';                                                                                                                      
+      i1++;  
+      i2++;                                                                                                                                 
+    }                                                                                                                                         
+  concat[i2] = '\0';                                                                                                                          
+  return (concat);                                                                                                                            
+                                                                                                                                              
+    }  
